fba02e409b4e6c71d301b29790313b3e7c3578b0
pushingLoading <pavaoduzevic@outlook.com>
2018-09-24 13:04:07 +0200

    map zoom

--- app/src/main/java/com/pg/citybulb/citybulb/CityMapActivity.java
+++ app/src/main/java/com/pg/citybulb/citybulb/CityMapActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,8 +40,18 @@ public class CityMapActivity extends FragmentActivity implements OnMapReadyCallb
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.getUiSettings().setCompassEnabled(false);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(39.87266,-4.028275))
+                .zoom(18)
+                .tilt(67.5f)
+                .bearing(314)
+                .build();
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
